a72c621d78a1df2349b0af2d44c3396093ede142
mihyeon <2_5_3_1@naver.com>
2021-10-08 10:13:37 +0900

    udate

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles/index.css";
+
+const App = () => {
+    return (
+    <div className="App   h-[100vh] flex  flex-col">
+        <div className="w-[10vw] h-[10vw] border hover:border hover:border-red-100"></div>
+    </div>
+    );
+};
+
+export default App;
